43bf2ba2541ea13906e72814166104cea421ba22
SheaLine <sline@ucsc.edu>
2024-11-04 17:53:18 -0800

    small change

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 // todo
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameTitle: string = "My Game";
+document.title = gameTitle;
+
+const header = document.createElement("h1");
+header.innerHTML = gameTitle;
+app.append(header);
+
+const alertButton = document.createElement("button")!;
+alertButton.innerHTML = "Click me!";
+app.append(alertButton);
+
+alertButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
